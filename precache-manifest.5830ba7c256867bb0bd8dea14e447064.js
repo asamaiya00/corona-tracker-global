8a86dde1220149135692cd7a66ef216880c00412
asamaiya00 <asamaiya00@gmail.com>
2020-07-20 13:35:05 +0530

    Updates

---
+++ precache-manifest.5830ba7c256867bb0bd8dea14e447064.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e9e41446f24d19fab8e7c2b360b8085f",
+    "url": "/corona-tracker-global/index.html"
+  },
+  {
+    "revision": "003ddeab77d688d1c6ab",
+    "url": "/corona-tracker-global/static/css/main.d087e1a7.chunk.css"
+  },
+  {
+    "revision": "322a88214a3c30997ad0",
+    "url": "/corona-tracker-global/static/js/2.f53cbd0c.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/corona-tracker-global/static/js/2.f53cbd0c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "003ddeab77d688d1c6ab",
+    "url": "/corona-tracker-global/static/js/main.d682a5e4.chunk.js"
+  },
+  {
+    "revision": "245245ceab1dabeee9ee",
+    "url": "/corona-tracker-global/static/js/runtime-main.0e3f8079.js"
+  },
+  {
+    "revision": "ef290da19c8ffa490dda770631fa7e21",
+    "url": "/corona-tracker-global/static/media/covid_new.ef290da1.png"
+  }
+]);
